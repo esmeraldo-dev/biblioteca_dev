6a6424efba276e606fbf40b74cdb3f4e0d30f193
Vinicius Esmeraldo <vinicius.esmeraldodev@gmail.com>
2026-01-22 21:57:55 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/vinicius/biblioteca_dev/BibliotecaDevApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.biblioteca_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/vinicius/biblioteca_dev/BibliotecaDevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.biblioteca_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
