5bd9cf616ba59947a65d66eeba830d6cd78d66c9
Vinicius Esmeraldo <vinicius.esmeraldodev@gmail.com>
2026-01-22 23:37:49 -0300

    Adicionar mapeamento das entidades principais (Livro, Emprestimo e Autor)

---
+++ src/main/java/br/com/vinicius/biblioteca_dev/entities/Livro.java
@@ -0,0 +1,41 @@
+package br.com.vinicius.biblioteca_dev.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_livros")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String titulo;
+
+    @Column(unique = true, nullable = false)
+    private String isbn;
+
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+}
---
+++ src/main/java/br/com/vinicius/biblioteca_dev/services/EmprestimoService.java
@@ -0,0 +1,7 @@
+package br.com.vinicius.biblioteca_dev.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmprestimoService {
+}
